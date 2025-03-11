5a062634840b5080219ea32229b65ac67aa9899d
Marcin Kolbe <marcin.kolbe@gmail.com>
2025-03-11 18:21:57 +0100

    JDP250201-12: dodano encję Product

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "PRODUCTS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @Column(name = "PRICE", nullable = false)
+    private BigDecimal price;
+
+//    !!! Do czasu dodania encji Group relacja musi być zakomentowana żeby projekt się budował. !!!
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "GROUP_ID", nullable = false)
+//    private Group group;
+}
