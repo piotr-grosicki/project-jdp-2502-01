b599dac4f679badd5ee6db8482b0f714db20a84a
Roman <roman.adamczyk12@gmail.com>
2025-03-11 14:50:23 +0100

    Entity Cart addition and relation with other entities definitions

---
+++ src/main/java/com/kodilla/ecommercee/domain/Users.java
@@ -0,0 +1,27 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "USERS")
+public class Users {
+
+    private int id;
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
