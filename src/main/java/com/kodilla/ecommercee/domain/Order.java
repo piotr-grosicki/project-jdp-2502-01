ec69722a18bd9e70459e024e6b0df379b4701d50
Michal Kryszewski <m.kryszewski@gmail.com>
2025-03-13 19:47:07 +0100

    JDP250201-11: dodany kod

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ORDERS")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER ID")
+    private int id;
+
+    @Column(name = "DATE OF ORDER", nullable = false)
+    private LocalDate orderDate;
+
+    @Column(name = "USER ID", nullable = false)
+    private int userId;
+
+    @Column(name = "TOTAL PRICE", nullable = false)
+    private double totalPrice;
+
+    /*
+    Relacja zakomentowana w oczekiwaniu na klasę User
+
+    @ManyToOne
+    @JoinColumn(name = "USER ID", referencedColumnName = "USER ID", insertable = false, updatable = false)
+    private User user;
+     */
 }
+
