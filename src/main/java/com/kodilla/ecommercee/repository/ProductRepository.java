864dda227b18d3e5f13ed6b0c07def2e102abf81
Michal Kryszewski <m.kryszewski@gmail.com>
2025-03-17 19:52:36 +0100

    JDP250201-19: testy encji Cart + UserController/UserDto moved to controller/domain

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -1,21 +1,18 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.domain.Cart;
-import jakarta.transaction.Transactional;
+import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
 @Repository
-public interface CartRepository extends CrudRepository<Cart, Integer> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
     @Override
-    List<Cart> findAll();
+    List<Product> findAll();
 
     @Override
-    Optional findById(Integer id);
-
+    Optional<Product> findById(Long id);
 }
