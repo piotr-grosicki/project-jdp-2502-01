c556784d8993736f4bf30cf593beca1eaeefa5d2
Michał Mielcarek <mike249mielcarek@gmail.com>
2025-03-23 10:27:14 +0100

    Changed mapping in product and crudRepos to Jparepos in repositories

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -2,8 +2,8 @@ package com.kodilla.ecommercee.repository;
 
 
 import com.kodilla.ecommercee.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface GroupRepository extends CrudRepository<Group, Long> {
+public interface GroupRepository extends JpaRepository<Group, Long> {
 
     @Override
     List<Group> findAll();
