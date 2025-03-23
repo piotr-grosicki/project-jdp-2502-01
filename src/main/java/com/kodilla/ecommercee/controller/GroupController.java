63548b3030acd6fe4bc4609bd434b6ee94abdb7d
tron <tomasz26bojko@gmail.com>
2025-03-23 13:55:25 +0100

    "zmieniono endpoint ma /v1/groups"

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/groups")
+@RequestMapping("/v1/groups")
 @AllArgsConstructor
 public class GroupController {
 
