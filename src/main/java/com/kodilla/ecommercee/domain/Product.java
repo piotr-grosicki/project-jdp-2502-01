ef99be93779bb6cca205c8cd045939ba8f398273
Michal Kryszewski <m.kryszewski@gmail.com>
2025-03-24 15:51:23 +0100

    JDP250201-19: poprawione testy

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -40,4 +41,18 @@ public class Product {
     @JoinColumn(name = "GROUP_ID", nullable = false)
     @JsonIgnore
     private Group group;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
